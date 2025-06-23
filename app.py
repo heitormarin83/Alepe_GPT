b719998170bbaeb9bfe4fe4bd18eafb19f73be5f
heitormarin83 <heitor.a.marin@gmail.com>
2025-06-23 18:04:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
-from main import executar_robot
+from fastapi import FastAPI, Query
+from main import executar_robot_parametrizado
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"status": "OK", "message": "Alepe GPT API rodando."}
 
+
 @app.get("/run")
-def run_robot():
-    resultado = executar_robot()
+def run_robot(
+    docid: str = Query(..., description="Código da proposição"),
+    tipoprop: str = Query(..., description="Tipo da proposição, ex: p")
+):
+    resultado = executar_robot_parametrizado(docid, tipoprop)
     return resultado
