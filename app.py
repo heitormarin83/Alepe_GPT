15494611c2e0ddb69007d31118253ec905624d27
heitormarin83 <heitor.a.marin@gmail.com>
2025-06-23 21:34:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from main import executar_robot
 
 app = FastAPI()
@@ -6,10 +6,14 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "Alepe GPT API funcionando"}
+    return {"status": "OK", "message": "Alepe GPT API rodando."}
 
 
 @app.get("/run")
-def run_robot():
-    resultado = executar_robot("projetos", "3005", "2025")
+def run_robot(
+    proposicao: str = Query(None, description="Tipo da proposição, ex: projetos"),
+    numero: str = Query(None, description="Número da proposição"),
+    ano: str = Query(None, description="Ano da proposição")
+):
+    resultado = executar_robot(proposicao=proposicao, numero=numero, ano=ano)
     return resultado
